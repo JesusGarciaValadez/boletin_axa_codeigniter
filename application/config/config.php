067c547c31823630bbf6082f3287d18f58bac474
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2015-01-16 13:15:37 -0600

    Configuration for debug messages

--- application/config/config.php
+++ application/config/config.php
@@ -180,7 +180,7 @@ $config['directory_trigger']	= 'd'; // experimental not currently in use
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 2;
+$config['log_threshold'] = 3;
 
 /*
 |--------------------------------------------------------------------------
@@ -244,9 +244,9 @@ $config['encryption_key'] = '4ae0d09721732e0a9db9bc7686ff832eec7e5edc7142153fd88
 | 'sess_time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
-$config['sess_cookie_name']		= 'ci_session';
+$config['sess_cookie_name']		= 'axa_session';
 $config['sess_expiration']		= 7200;
-$config['sess_expire_on_close']	= FALSE;
+$config['sess_expire_on_close']	= TRUE;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= FALSE;
 $config['sess_table_name']		= 'ci_sessions';
