3b597afee447e2736452c56b034e15c2745e2081
Jesús García <zerovamp@hotmail.com>
2015-03-24 21:56:55 +0000

    Markup modified for health section

--- application/views/2015/abril/salud_v.php
+++ application/views/2015/abril/salud_v.php
@@ -18,142 +18,144 @@
         <li><?=anchor('opinion', 'TU OPINI&Oacute;N');?></li>
     </ul>
 </nav>
-<div class="container health">
-    <div class="frame title">
-        <div class="bit-100">
+<div class="container health clearfix">
+    <div class="frame title clearfix bit-1">
+        <div class="bit-1">
             <h1 class="bit-60"><strong><span>11</span> razones para <br />dejar de fumar</strong></h1>
         </div>
         <div class="bit-60">
-            <p class="bit-100">El hábito de fumar es la principal causa evitable de enfermedades como cáncer y afecciones cardiorrespiratorias y provoca más fallecimientos prematuros que los que ocasionan en conjunto el SIDA, la drogadicción, el alcoholismo, los accidentes viales, los incendios e incluso los homicidios, según datos de la Organización Mundial de la Salud.</p>
+            <p class="bit-1"><strong>El hábito de fumar es la principal causa evitable de enfermedades como cáncer y afecciones cardiorrespiratorias y provoca más fallecimientos prematuros que los que ocasionan en conjunto el SIDA, la drogadicción, el alcoholismo, los accidentes viales, los incendios e incluso los homicidios, según datos de la Organización Mundial de la Salud.</strong></p>
         </div>
         <div class="bit-40">
             <?=img('images/assets/2015/abril/salud/no-fumar.png');?>
         </div>
     </div>
-    <div class="frame section-1">
-        <div class="bit-100">
-            <h2 class="bit-80">¿Vale la pena poner en riesgo tu salud?</h2>
-            <p class="bit-80">Te damos 11 razones para dejar de fumar y mejorar tu calidad de vida:</p>
+    <div class="frame section-1 bit-1">
+        <div class="bit-1">
+            <h2 class="bit-1">¿Vale la pena poner en riesgo tu salud?</h2>
+            <p class="bit-1">Te damos 11 razones para dejar de fumar y mejorar tu calidad de vida:</p>
         </div>
-        <div class="bit-100">
-            <ul class="bit-80">
-                <li class="bit-20"><?=img('images/assets/2015/abril/salud/1.png');?></li>
+        <div class="bit-1">
+            <ul class="bit-1 clearfix">
+                <li class="bit-5"><?=img('images/assets/2015/abril/salud/1.png');?></li>
                 <li class="bit-60">
-                    <p class="bit-100"><strong>Mejora tu presión arterial</strong>, aumentas la oxigenación al corazón y reduces el riesgo de sufrir un infarto. A los 10 años de abandonar este vicio la probabilidad de enfermedad coronaria es prácticamente la misma que la de cualquier persona que nunca haya fumado.</p>
+                    <p class="bit-1"><strong>Mejora tu presión arterial</strong>, aumentas la oxigenación al corazón y reduces el riesgo de sufrir un infarto. A los 10 años de abandonar este vicio la probabilidad de enfermedad coronaria es prácticamente la misma que la de cualquier persona que nunca haya fumado.</p>
                 </li>
-                <li class="bit-20"><?=img('images/assets/2015/abril/salud/presion.png');?></li>
+                <li class="bit-5"><?=img('images/assets/2015/abril/salud/presion.png');?></li>
             </ul>
         </div>
-        <div class="bit-100">
-            <ul class="bit-80">
-                <li class="bit-20"><?=img('images/assets/2015/abril/salud/2.png');?></li>
+        <div class="bit-1">
+            <ul class="bit-1 clearfix">
+                <li class="bit-5"><?=img('images/assets/2015/abril/salud/2.png');?></li>
                 <li class="bit-60">
-                    <p class="bit-100"><strong>Reparas tu digestión</strong>, reduces las úlceras producidas por el tabaco e incluso percibes mejor el sabor de los alimentos y los olores.</p>
+                    <p class="bit-1"><strong>Reparas tu digestión</strong>, reduces las úlceras producidas por el tabaco e incluso percibes mejor el sabor de los alimentos y los olores.</p>
                 </li>
-                <li class="bit-20"><?=img('images/assets/2015/abril/salud/estomago.png');?></li>
+                <li class="bit-5"><?=img('images/assets/2015/abril/salud/estomago.png');?></li>
             </ul>
         </div>
-        <div class="bit-100">
-            <ul class="bit-80">
-                <li class="bit-20"><?=img('images/assets/2015/abril/salud/3.png');?></li>
+        <div class="bit-1">
+            <ul class="bit-1 clearfix">
+                <li class="bit-5"><?=img('images/assets/2015/abril/salud/3.png');?></li>
                 <li class="bit-60">
-                    <p class="bit-100"><strong>Tu piel se regenera</strong>: evitas el envejecimiento prematuro, arrugas, caída del cabello y el tono opaco.</p>
+                    <p class="bit-1"><strong>Tu piel se regenera</strong>: evitas el envejecimiento prematuro, arrugas, caída del cabello y el tono opaco.</p>
                 </li>
-                <li class="bit-20"><?=img('images/assets/2015/abril/salud/muela.png');?></li>
+                <li class="bit-5"><?=img('images/assets/2015/abril/salud/muela.png');?></li>
             </ul>
         </div>
-        <div class="bit-100">
-            <ul class="bit-80">
-                <li class="bit-20"><?=img('images/assets/2015/abril/salud/4.png');?></li>
+        <div class="bit-1">
+            <ul class="bit-1 clearfix">
+                <li class="bit-5"><?=img('images/assets/2015/abril/salud/4.png');?></li>
                 <li class="bit-60">
-                    <p class="bit-100"><strong>Mejora el aspecto</strong> de tus dientes y encías, sobre todo, tu aliento.</p>
+                    <p class="bit-1"><strong>Mejora el aspecto</strong> de tus dientes y encías, sobre todo, tu aliento.</p>
                 </li>
-                <li class="bit-20"><?=img('images/assets/2015/abril/salud/bicicleta.png');?></li>
+                <li class="bit-5"><?=img('images/assets/2015/abril/salud/bicicleta.png');?></li>
             </ul>
         </div>
-        <div class="bit-100">
-            <ul class="bit-80">
-                <li class="bit-20"><?=img('images/assets/2015/abril/salud/5.png');?></li>
+        <div class="bit-1">
+            <ul class="bit-1 clearfix">
+                <li class="bit-5"><?=img('images/assets/2015/abril/salud/5.png');?></li>
                 <li class="bit-60">
-                    <p class="bit-100"><strong>Mejora la capacidad pulmonar</strong> y circulación, recuperando energía y vitalidad para realizar esfuerzos físicos.</p>
+                    <p class="bit-1"><strong>Mejora la capacidad pulmonar</strong> y circulación, recuperando energía y vitalidad para realizar esfuerzos físicos.</p>
                 </li>
-                <li class="bit-20"><?=img('images/assets/2015/abril/salud/corazon.png');?></li>
+                <li class="bit-5"><?=img('images/assets/2015/abril/salud/corazon.png');?></li>
             </ul>
         </div>
-        <div class="bit-100">
-            <ul class="bit-80">
-                <li class="bit-20"><?=img('images/assets/2015/abril/salud/6.png');?></li>
+        <div class="bit-1">
+            <ul class="bit-1 clearfix">
+                <li class="bit-5"><?=img('images/assets/2015/abril/salud/6.png');?></li>
                 <li class="bit-60">
-                    <p class="bit-100"><strong>La posibilidad de contraer gripe</strong> y el riesgo de sufrir enfermedades coronarias se reduce a un 50%.</p>
+                    <p class="bit-1"><strong>La posibilidad de contraer gripe</strong> y el riesgo de sufrir enfermedades coronarias se reduce a un 50%.</p>
                 </li>
-                <li class="bit-20"><?=img('images/assets/2015/abril/salud/grafica.png');?></li>
+                <li class="bit-5"><?=img('images/assets/2015/abril/salud/grafica.png');?></li>
             </ul>
         </div>
-        <div class="bit-100">
-            <ul class="bit-80">
-                <li class="bit-20"><?=img('images/assets/2015/abril/salud/7.png');?></li>
+        <div class="bit-1">
+            <ul class="bit-1 clearfix">
+                <li class="bit-5"><?=img('images/assets/2015/abril/salud/7.png');?></li>
                 <li class="bit-60">
-                    <p class="bit-100"><strong>Después de cinco años de no fumar,</strong> el riesgo de cáncer de pulmón se reduce a la mitad.</p>
+                    <p class="bit-1"><strong>Después de cinco años de no fumar,</strong> el riesgo de cáncer de pulmón se reduce a la mitad.</p>
                 </li>
-                <li class="bit-20"><?=img('images/assets/2015/abril/salud/espermatozooides.png');?></li>
+                <li class="bit-5"><?=img('images/assets/2015/abril/salud/espermatozooides.png');?></li>
             </ul>
         </div>
-        <div class="bit-100">
-            <ul class="bit-80">
-                <li class="bit-20"><?=img('images/assets/2015/abril/salud/8.png');?></li>
+        <div class="bit-1">
+            <ul class="bit-1 clearfix">
+                <li class="bit-5"><?=img('images/assets/2015/abril/salud/8.png');?></li>
                 <li class="bit-60">
-                    <p class="bit-100"><strong>Disminuye el riesgo de sufrir cáncer</strong> de pulmón, laringe, vejiga, boca, esófago y riñón.</p>
+                    <p class="bit-1"><strong>Disminuye el riesgo de sufrir cáncer</strong> de pulmón, laringe, vejiga, boca, esófago y riñón.</p>
                 </li>
-                <li class="bit-20"><?=img('images/assets/2015/abril/salud/moneda.png');?></li>
+                <li class="bit-5"><?=img('images/assets/2015/abril/salud/moneda.png');?></li>
             </ul>
         </div>
-        <div class="bit-100">
-            <ul class="bit-80">
-                <li class="bit-20"><?=img('images/assets/2015/abril/salud/9.png');?></li>
+        <div class="bit-1">
+            <ul class="bit-1 clearfix">
+                <li class="bit-5"><?=img('images/assets/2015/abril/salud/9.png');?></li>
                 <li class="bit-60">
-                    <p class="bit-100"><strong>El riesgo de padecer</strong> disfunción eréctil disminuye.</p>
+                    <p class="bit-1"><strong>El riesgo de padecer</strong> disfunción eréctil disminuye.</p>
                 </li>
-                <li class="bit-20"><?=img('images/assets/2015/abril/salud/presion.png');?></li>
+                <li class="bit-5"><?=img('images/assets/2015/abril/salud/presion.png');?></li>
             </ul>
         </div>
-        <div class="bit-100">
-            <ul class="bit-80">
-                <li class="bit-20"><?=img('images/assets/2015/abril/salud/10.png');?></li>
+        <div class="bit-1">
+            <ul class="bit-1 clearfix">
+                <li class="bit-5"><?=img('images/assets/2015/abril/salud/10.png');?></li>
                 <li class="bit-60">
-                    <p class="bit-100"><strong>¡Ahorras!</strong> No sólo en la compra del tabaco, sino en medicinas, visitas al médico y gripes por tabaquismo.</p>
+                    <p class="bit-1"><strong>¡Ahorras!</strong> No sólo en la compra del tabaco, sino en medicinas, visitas al médico y gripes por tabaquismo.</p>
                 </li>
-                <li class="bit-20"><?=img('images/assets/2015/abril/salud/presion.png');?></li>
+                <li class="bit-5"><?=img('images/assets/2015/abril/salud/presion.png');?></li>
             </ul>
         </div>
-        <div class="bit-100">
-            <ul class="bit-80">
-                <li class="bit-20"><?=img('images/assets/2015/abril/salud/11.png');?></li>
+        <div class="bit-1">
+            <ul class="bit-1 clearfix">
+                <li class="bit-5"><?=img('images/assets/2015/abril/salud/11.png');?></li>
                 <li class="bit-60">
-                    <p class="bit-100"><strong>Al no fumar preservas la salud</strong> de los no fumadores</p>
+                    <p class="bit-1"><strong>Al no fumar preservas la salud</strong> de los no fumadores</p>
                 </li>
-                <li class="bit-20"><?=img('images/assets/2015/abril/salud/viejita.png');?></li>
+                <li class="bit-5"><?=img('images/assets/2015/abril/salud/viejita.png');?></li>
             </ul>
         </div>
     </div>
-    <div class="frame section-2">
-        <h3 class="bit-80">Probablemente conocías los riesgos, ahora conoces algunos beneficios: <strong>mantenerlos es tu decisión</strong>.</p>
+    <div class="frame section-2 bit-1">
+        <h3 class="bit-1">Probablemente conocías los riesgos, ahora conoces algunos beneficios: <strong>mantenerlos es tu decisión</strong>.</h3>
     </div>
-    <div class="frame section-3">
-        <h4 class="bit-80"><strong>¡Agrega días a tu vida y a la de los tuyos!</strong></h4>
+    <div class="frame section-3 bit-1">
+        <h4 class="bit-1"><strong>¡Agrega días a tu vida y a la de los tuyos!</strong></h4>
     </div>
-    <div class="frame section-4">
-        <p class="bit-3"><sup>*</sup>Fuente: trendenciashombre.com <br />bvs.sld.cu/revistas/med <br />Organización Mundial de la Salud (OMS)</p>
+    <div class="frame section-4 bit-1">
+        <p class="bit-3"><sup>*</sup> Fuente: <?=anchor( 'http://www.trendenciashombre.com' );?> <br /><?=anchor( 'http://bvs.sld.cu/revistas/med/' );?> <br />Organización Mundial de la Salud (OMS)</p>
     </div>
-    <div class="frame more--information">
-        <h5>Si necesitas más información</h5>
-        <h5>¡Contacta a tu agente!</h5>
+    <hr class="bit-1 rule-information bit-1" />
+    <div class="frame more--information bit-1">
+        <h5 class="bit-1">Si necesitas más información</h5>
+        <h5 class="bit-1">¡Contacta a tu agente!</h5>
     </div>
-    <div class="frame related">
-        <div class="bit-3">
-            <h6>Artículos relacionados</h6>
-            <ul>
-                <li><?=anchor('http://www.boletinaxa.com.mx/anterior/tu-salud-junio-2014.html','Nutrición: pieza clave en la prevención');?></li>
-                <li><?=anchor('http://www.boletinaxa.com.mx/anterior/tu-salud-marzo-2014.html','La obesidad más allá del sobrepeso');?></li>
+    <div class="frame related bit-1">
+        <div class="bit-3 clearfix">
+            <h6 class="bit-1">Artículos relacionados</h6>
+            <ul class="bit-1">
+                <li class="bit-1"><?=anchor('http://www.boletinaxa.com.mx/anterior/tu-salud-junio-2014.html','Nutrición: pieza clave en la prevención');?></li>
+                <li class="bit-1"><?=anchor('http://www.boletinaxa.com.mx/anterior/tu-salud-marzo-2014.html','La obesidad más allá del sobrepeso');?></li>
             </ul>
         </div>
     </div>
+</div>
