95ea2b7b54c207c9ff331db90c704a6b77bbe7dd
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2015-03-30 20:08:26 +0000

    Corrections in some classes in auto view april 2015

--- application/views/2015/abril/auto_v.php
+++ application/views/2015/abril/auto_v.php
@@ -8,7 +8,6 @@
         <p>Consulta los n&uacute;meros anteriores <?=anchor('http://www.boletinaxa.com.mx/anterior/historico.html', img('images/assets/btn_landing.png'));?></p>
     </div>
 </header>
-
 <nav class="frame">
     <ul>
         <li><?=anchor('/', 'INICIO');?></li>
@@ -19,162 +18,161 @@
         <li><?=anchor('opinion?email='.$this->session->userdata('email'), 'TU OPINI&Oacute;N');?></li>
     </ul>
 </nav>
-
 <div class="container auto">
-    <?=($this->session->flashdata('msg') ? '<p style="color:red;font-size:.8em;">'.$this->session->flashdata('msg').'</p>' : '');?>
-    <div class="frame darkblue"><br><br>
-        <div class="bit-1">
-            <h1 style="font-size:3.2em;">Manejar bien:</h1>
-            <h2 style="font-size:2em;">un propósito de Año Nuevo</h2>
+    <div class="frame bit-1 title clearfix">
+        <?=img( [ 'src' => 'images/assets/2015/abril/auto/head.png', 'alt' => 'Vacaciones sobre ruedas' ] );?>
+    </div>
+    <div class="bit-1 clearfix frame section-1">
+        <p>Ya tienes tus maletas hechas y has planeado tu ruta a algún destino de México: <strong>la primera temporada vacacional está por comenzar</strong>.</p>
+        <p>Sin embargo, hay ciertos aspectos que debes considerar antes de iniciar el viaje para evitar algún percance durante el trayecto:</p>
+    </div>
+    <div class="bit-1 clearfix frame section-2">
+        <div class="bit-20 clearfix">
+            <?= img( [ 'src' => 'images/assets/2015/abril/auto/manos-sosteniendo-un-auto.png', 'alt' => 'Auto' ] ); ?>
         </div>
-        <div class="bit-1">
-            <?=img('images/assets/auto_files/banner_azul.jpg');?>
-            <br>
+        <div class="bit-80 clearfix">
+            <h1>Para el auto:</h1>
+            <ul>
+                <li>Lleva tu auto al mecánico con anticipación a tu viaje. Haz que verifiquen los niveles de líquido de frenos, anticongelante, aceite de la transmisión, el sistema eléctrico, la suspensión y el funcionamiento en general. Esto evitará algún desperfecto o el sobrecalentamiento del vehículo.</li>
+            </ul>
         </div>
     </div>
-    <div class="frame">
-        <div class="bit-1 graytext">
-            <br>
-            <p style="font-size:1.2em;">Para la mayoría de las personas, el Año Nuevo representa el inicio de una etapa que propicia el cambio o la renovación de ciertas actitudes y hábitos. Generalmente, los propósitos están relacionados con el deporte, la salud y el ahorro.</p>
+    <div class="bit-1 clearfix frame section-3">
+        <div class="bit-2 clearfix">
+            <?= img( [ 'src' => 'images/assets/2015/abril/auto/presion-de-llanta.png', 'alt' => 'Tomando la presión de una llanta' ] ); ?>
+            <ul class="bit-80">
+                <li>Evita ponchaduras: revisa la presión de los neumáticos, principalmente si tu destino se halla en un medio de terracería.</li>
+            </ul>
         </div>
-        <div class="bit-1">
-            <div align="center">
-                <?=img('images/assets/auto_files/iconos_amarillos.png');?>
-                <br>
-            </div>
-        </div>
-    </div>
-    <div class="frame">
-        <div class="bit-1">
-            <h1  style="font-size:3em;">Pero...</h1>
-        </div>
-        <div class="bit-1">
-            <p style="font-size:1.4em;">¿por qué no pensar en acciones concretas que puedan impactar de manera positiva en
-nuestra sociedad?</p>
-        </div>
-        <div class="bit-1">
-            <?=img('images/assets/auto_files/banner_3.png');?><br>
+        <div class="bit-2 clearfix">
+            <?= img( [ 'src' => 'images/assets/2015/abril/auto/paraguas-tapando-auto.png', 'alt' => 'Paraguas tapando auto' ] ); ?>
+            <ul class="bit-80">
+                <li>Evita derrapes: revisa el desgaste de las llantas, es importante en caso de enfrentarte a lluvias o zonas húmedas.</li>
+            </ul>
         </div>
     </div>
-    <div class="frame">
-        <div class="bit-1">
-            <p style="font-size:1.4em;">Cada año, por la inseguridad vial, 16 mil mexicanos pierden la vida y más de un millón de niños, jóvenes y adultos sufren discapacidad, lesiones u orfandad.</p>
-            <br>
-            <p style="font-size:1.4em;"><strong>Las principales causas de accidentes vehiculares fatales en México son:</strong></p>
+    <div class="bit-1 clearfix frame section-4">
+        <div class="bit-2 clearfix">
+            <?= img( [ 'src' => 'images/assets/2015/abril/auto/llanta-desinflada.png', 'alt' => 'Paraguas tapando auto' ] ); ?>
+            <ul class="bit-80">
+                <li>Si las llantas presentan pinchaduras o chipotes es necesario reemplazarlos.</li>
+            </ul>
+        </div>
+        <div class="bit-2 clearfix">
+            <?= img( [ 'src' => 'images/assets/2015/abril/auto/llave-de-tuercas.png', 'alt' => 'Paraguas tapando auto' ] ); ?>
+            <ul class="bit-80">
+                <li>Revisa la condición de la llanta de refacción, así como contar con las herramientas necesarias.</li>
+            </ul>
         </div>
     </div>
-    <div class="frame">
-        <div class="bit-3" style="text-align:right;">
-            <h1 style="font-size:5em; color:#039">60%</h1>
-            <p style="font-size:2em;color:#039">Alcohol</p>
-            <p style="color:#039">Consumir alcohol por arriba de los límites legales y manejar es la principal causa de estos accidentes</p><br>
-            <p style="color:#039">El índice de <strong>uso peligroso de consumo del alcohol </strong>en México duplica al de EUA y es 33% mayor al promedio Latinoamericano</p>
-            <br>
+    <div class="bit-1 clearfix frame section-5">
+        <div class="bit-20 clearfix">
+            <?= img( [ 'src' => 'images/assets/2015/abril/auto/mapa.png', 'alt' => 'Mapa' ] ); ?>
         </div>
-        <div class="bit-3" style="text-align:center;">
-            <br>
-            <?=img('images/assets/auto_files/grafica.png');?>
-        </div>
-        <div class="bit-3" style="text-align:left;">
-            <h3><span style="font-size:3em; color:#CC3">28%</span> <span style="font-size:1.8em; color:#CC3">Velocidad</span></h3>
-            <p style="color:#CC3">El exceso de velocidad es la segunda causa de estos accidentes</p><br><br>
-            <h3> <span style="font-size:2em; color:#666">12%</span> <span style="font-size:1.6em; color:#666">Otras causas</span></h3>
-            <p style="color:#666">Los distractores, como el uso del celular, la falta de uso de cinturón de seguridad y sistemas de retención infantil, entre otras causas, ocasionan el resto de estos accidentes</p>
+        <div class="bit-80 clearfix">
+            <h2>Para el camino:</h2>
+            <ul>
+                <li>Planea tu ruta y calcula los tiempos, así podrás tomar descansos breves durante el viaje y evitar el tránsito lento.</li>
+            </ul>
         </div>
     </div>
-    <div class="frame">
-        <div class="bit-1">
-            <p><span class="source graytext">Fuente: Instituto Nacional de Psiquiatría, 2010 y CONAPRA, 2008.</span></p>
+    <div class="bit-1 clearfix frame section-6">
+        <div class="bit-3 clearfix">
+            <?= img( [ 'src' => 'images/assets/2015/abril/auto/sol.png', 'alt' => 'Sol' ] ); ?>
+            <ul>
+                <li>Infórmate acerca de las últimas noticias sobre el clima en la zona de tu destino, así como de la condición de las vialidades.</li>
+            </ul>
+        </div>
+        <div class="bit-3 clearfix">
+            <?= img( [ 'src' => 'images/assets/2015/abril/auto/stop.png', 'alt' => 'Señal de alto' ] ); ?>
+            <ul>
+                <li>Utiliza las vías de comunicación oficiales, evita rutas alternas que te sean desconocidas y no te estaciones en lugares solitarios.</li>
+            </ul>
+        </div>
+        <div class="bit-3 clearfix">
+            <?= img( [ 'src' => 'images/assets/2015/abril/auto/bebe.png', 'alt' => 'Bebé' ] ); ?>
+            <ul>
+                <li>Usa siempre el cinturón de seguridad. Si viajas con bebés o niños menores a 3 años deberán viajar con silla de seguridad y los mayores de 3 años irán en el asiento trasero con cinturón de seguridad.</li>
+            </ul>
         </div>
     </div>
-    <br>
-    <div class="frame darkblue">
-        <br>
-        <div class="bit-2" style="padding:0px;">
-		  <h3 style="text-align:left; padding-left: 20px">Está demostrado que si seguimos estas cinco conductas salvavidas podemos reducir de forma importante las muertes y lesiones asociadas a la violencia vial:</h3><br>
-		  <div><br>
-		      <?=img( array( 'src' => 'images/assets/auto_files/crash_dummy.png', 'style' => "max-height:100%") );?>
-		  </div>
+    <div class="bit-1 clearfix frame section-7">
+        <div class="bit-3 clearfix">
+            <?= img( [ 'src' => 'images/assets/2015/abril/auto/candado.png', 'alt' => 'Candado' ] ); ?>
+            <ul>
+                <li>Utiliza el seguro de niños en las puertas y ventanas. Es recomendable que un adulto viaje con los niños en el asiento trasero.</li>
+            </ul>
+        </div>
+        <div class="bit-3 clearfix">
+            <?= img( [ 'src' => 'images/assets/2015/abril/auto/botella.png', 'alt' => 'Botella' ] ); ?>
+            <ul>
+                <li>Evita el consumo de alcohol antes o durante el viaje.</li>
+            </ul>
+        </div>
+        <div class="bit-3 clearfix">
+            <?= img( [ 'src' => 'images/assets/2015/abril/auto/limites-de-velocidad.png', 'alt' => 'Limtes de velocidad' ] ); ?>
+            <ul>
+                <li>Respeta los límites de velocidad. Reduce la velocidad si el clima es adverso por lluvia, tormenta, neblina u otra situación.</li>
+            </ul>
+        </div>
     </div>
-    <div class="bit-2" style="padding-bottom:0px;">
-        <div class="frame">
-            <div class="bit-40">
-                <?=img('images/assets/auto_files/icono_No_1.png');?>
-            </div>
-            <div class="bit-60"><br><br>
-                <h3 style="text-align:left">1. Si vas a manejar, no tomes.</h3>
-            </div>
+    <div class="bit-1 clearfix frame section-8">
+        <div class="bit-3 clearfix">
+            <?= img( [ 'src' => 'images/assets/2015/abril/auto/semaforo.png', 'alt' => 'Semáforo' ] ); ?>
+            <ul>
+                <li>Respeta los señalamientos. Evita obstruir la circulación a otro vehículo.</li>
+            </ul>
         </div>
-        <div class="frame">
-            <div class="bit-40">
-                <?=img('images/assets/auto_files/icono_No_2.png');?>
-            </div>
-            <div class="bit-60">
-                <br><br>
-                <h3 style="text-align:left">2. Respeta los límites de velocidad.</h3>
-            </div>
+        <div class="bit-3 clearfix">
+            <?= img( [ 'src' => 'images/assets/2015/abril/auto/coche-viajando-de-noche.png', 'alt' => 'Coche viajando de noche' ] ); ?>
+            <ul>
+                <li>Evita conducir cansado, enfermo o enojado.</li>
+            </ul>
         </div>
-        <div class="frame">
-            <div class="bit-40">
-                <?=img('images/assets/auto_files/icono_No_3.png');?>
-            </div>
-            <div class="bit-60"><br><br>
-                <h3 style="text-align:left">3. Usa cinturón de seguridad y sillas para niños y bebés.</h3>
-            </div>
+        <div class="bit-3 clearfix">
+            <?= img( [ 'src' => 'images/assets/2015/abril/auto/signo-de-alerta.png', 'alt' => 'Signo de alerta' ] ); ?>
+            <ul>
+                <li>Mantente alerta: no distraigas tu atención del camino u otros vehículos alrededor.</li>
+            </ul>
         </div>
-        <div class="frame">
-            <div class="bit-40">
-                <?=img('images/assets/auto_files/icono_No_4.png');?>
-            </div>
-            <div class="bit-60"><br><br>
-                <h3 style="text-align:left">4. Evita distracciones como el uso del teléfono mientras manejas.</h3>
-            </div>
+    </div>
+    <div class="bit-1 clearfix frame section-9">
+        <div class="bit-3 clearfix">
+            <?= img( [ 'src' => 'images/assets/2015/abril/auto/coche-con-luces--de-emergencia-prendidas.png', 'alt' => 'Luces de emergencia' ] ); ?>
+            <ul>
+                <li>En caso de algún desperfecto mecánico enciende las luces de emergencia, oríllate y contacta al equipo de auxilio vial de la concesionaria.</li>
+            </ul>
         </div>
-        <div class="frame">
-            <div class="bit-40">
-                <?=img('images/assets/auto_files/icono_No_5.png');?>
-            </div>
-            <div class="bit-60"><br><br>
-                <h3 style="text-align:left">5. Usa casco al viajar en moto y procura usarlo en bicicleta.</h3>
-            </div>
+        <div class="bit-3 clearfix">
+            <?= img( [ 'src' => 'images/assets/2015/abril/auto/celular.png', 'alt' => 'Teléfono celular' ] ); ?>
+            <ul>
+                <li>No uses el celular, no fumes ni lleves la música a alto volumen al conducir, esto disminuye la capacidad de concentración hasta en un 50%.</li>
+            </ul>
+        </div>
+        <div class="bit-3 clearfix">
+            <?= img( [ 'src' => 'images/assets/2015/abril/auto/maleta.png', 'alt' => 'Maleta' ] ); ?>
+            <ul>
+                <li>Si llevas equipaje en el asiento trasero no olvides sujetarlo con el cinturón de seguridad, esto evitará que se convierta en un proyectil en un accidente.</li>
+            </ul>
+        </div>
+    </div>
+    <div class="bit1 frame clearfix section-10">
+        <h3>Recuerda: la clave para unas vacaciones felices está en ir y re gresar sano y salvo. Prevé y protege tu vida y la de tu familia.</h3>
+        <p><sup>*</sup> Fuentes: noticias.universia.cl / gq.com.mx</p>
+    </div>
+    <hr class="bit-1 rule-information bit-1" />
+    <div class="frame more--information bit-1">
+        <h5 class="bit-1">Si necesitas más información</h5>
+        <h5 class="bit-1">¡Contacta a tu agente!</h5>
+    </div>
+    <div class="frame related bit-1">
+        <div class="bit-3 clearfix">
+            <h6 class="bit-1">Artículos relacionados</h6>
+            <ul class="bit-1">
+                <li class="bit-1"><?=anchor( 'http://www.boletinaxa.com.mx/anterior/tu-salud-junio-2014.html','Tú puedes ser tu propio jefe.' );?></li>
+                <li class="bit-1"><?=anchor( 'http://www.boletinaxa.com.mx/anterior/tu-salud-marzo-2014.html','México carece de una cultura del ahorro.' );?></li>
+            </ul>
         </div>
     </div>
 </div>
-<div class="frame">
-    <div class="bit-1"><br>
-        <h2 style="text-align: justify">En AXA te invitamos a reflexionar sobre estas conductas para que juntos evitemos que haya más víctimas de la inseguridad vial. Haz conciencia y agrega a tu lista de propósitos el manejar responsablemente.</h2>
-        <br>
-    </div>
-</div>
-<br>
-<div class="frame">
-    <div class="bit-1 blue-dotted"></div>
-</div>
-<div class="frame">
-    <div class="bit-3"></div>
 
-    <div class="bit-3">
-        <table class="related">
-            <thead>
-            <tr>
-                <th>Artículos relacionados</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td>
-                    <span class="pink-text">•</span> <?=anchor('http://www.boletinaxa.com.mx/anterior/tu-auto-diciembre-2013.html', 'Seguridad vial en México');?><br>
-                    <span class="pink-text">•</span> <?=anchor('http://www.boletinaxa.com.mx/2014/octubre/auto','Medidas preventivas en el funcionamiento de tu auto');?>
-                </td>
-            </tr>
-            </tbody>
-        </table>
-    </div>
-
-    <div class="bit-3"></div>
-</div>
-
-<div class="frame light-gray" style="text-align:center;margin-bottom:8px;">
-    <h2 class="lightblue-text">Si necesitas más información</h2>
-    <h2 class="pink-text">¡Contacta a tu agente!</h2>
-</div>
