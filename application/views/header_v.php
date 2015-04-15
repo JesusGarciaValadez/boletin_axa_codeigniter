b71dc488f44ed95fcf8a5d454616ac904971f042
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2015-04-15 23:00:36 +0000

    Reviewing markup inconsistencies in header and footer

--- application/views/header_v.php
+++ application/views/header_v.php
@@ -1,18 +1,23 @@
-<!DOCTYPE html>
+<?=doctype('html5');?>
 <html lang="es">
     <head>
         <title>Bolet&iacute;n Axa - <?=$mes;?></title>
-        <meta charset="utf-8">
-        <meta property="og:title" content="Bolet&iacute;n AXA <?=$mes;?> <?=$anio;?>" />
+        <meta charset='utf-8'>
+        <?=meta('description', '');?>
+        <meta property="og:title" content="Bolet&iacute;n AXA <?=$mes;?> <?=$anio;?>">
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="<?=base_url('images/assets/header.png');?>" />
-        <meta property="og:url" content="<?=current_url();?>" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-        <meta name="robots" content="no-index, no-follow" />
-        <base href="<?=base_url();?>" />
-        <link rel="shortcut icon" href="<?=base_url('images/assets/favicon.png');?>" type="image/png" />
-        <link rel="shortcut icon" href="<?=base_url('images/assets/favicon.ico');?>" type="image/x-icon" />
+        <meta property="og:image" content="<?=base_url('images/assets/header.png');?>">
+        <meta property="og:url" content="<?=current_url();?>">
+        <?=meta('viewport', 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no', 'name');?>
+        <?=meta('robots', 'no-index, no-follow', 'name');?>
+        <base href="<?=base_url();?>">
+
+        <?=link_tag( base_url('images/assets/favicon.png'), 'shortcut icon', 'image/png' );?>
+
+        <?=link_tag( base_url('images/assets/favicon.ico'), 'shortcut icon', 'image/x-icon' );?>
+
         <?=link_tag('css/{stylesheet}.css');?>
+
         <script>
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
