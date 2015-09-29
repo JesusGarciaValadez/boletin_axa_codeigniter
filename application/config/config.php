545c26fe546709b4f3f5f852149297b8434b1d61
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2015-09-29 22:09:23 +0000

    October 2015 - Session expire on close set to false

--- application/config/config.php
+++ application/config/config.php
@@ -246,7 +246,7 @@ $config['encryption_key'] = '4ae0d09721732e0a9db9bc7686ff832eec7e5edc7142153fd88
 */
 $config['sess_cookie_name']		= 'axa_session';
 $config['sess_expiration']		= 7200;
-$config['sess_expire_on_close']	= TRUE;
+$config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= FALSE;
 $config['sess_table_name']		= 'ci_sessions';
