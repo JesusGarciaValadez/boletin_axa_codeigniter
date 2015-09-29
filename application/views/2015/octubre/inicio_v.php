2f0424e33155f816fe7d8ede604e5228eda87f6c
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2015-09-29 22:13:13 +0000

    October 2015 - All the views for october 2015 - Home section markup done

---
+++ application/views/2015/octubre/inicio_v.php
@@ -0,0 +1,114 @@
+                <div class="container home">
+                    <div class="bit-1 frame clearfix title">
+                        <div class="frame">
+                            <div class="bit-70 rounded shadow blue-square clearfix" id="two-row-span">
+                                <div class="bit-40 clearfix">
+                                    <?=img( [ 'src' => 'images/assets/2015/julio/home/cama.png', 'title' => 'Cama', 'class' => 'bit-30' ] );?>
+                                    <?=img( [ 'src' => 'images/assets/2015/julio/home/lampara.png', 'title' => 'Lampara', 'class' => 'bit-30' ] );?>
+                                    <?=img( [ 'src' => 'images/assets/2015/julio/home/refrigerador.png', 'title' => 'Refrigerador' ] );?>
+                                    <?=img( [ 'src' => 'images/assets/2015/julio/home/lavadora.png', 'Lavadora', 'class' => 'bit-30' ] );?>
+                                    <?=img( [ 'src' => 'images/assets/2015/julio/home/bocinas.png', 'Bocinas', 'class' => 'bit-30' ] );?>
+                                </div>
+                                <div class="bit-60 clearfix">
+                                    <?=heading( 'Invertir', 1, 'class="bit-1 centered"' );?>
+                                    <?=heading( 'para darle valor al hogar', 2, 'class="bit-1"' );?>
+                                    <p class="bit-1">Equipar tu casa o departamento, además de darle personalidad a tu espacio, es la mejor forma de conservar o incrementar el valor de tu propiedad a través del tiempo.</p>
+                                    <p class="bit-50"><?=anchor( '2015/octubre/hogar', img( 'images/assets/2015/julio/home/leer-mas.png', 'Leer más' ) );?></p>
+                                </div>
+                            </div>
+                            <div class="bit-30 clearfix share-form">
+                                <div class="share bit-1 centered blue-square rounded shadow clearfix">
+                                    <h4 class="bit-60 clearfix">Compartir:</h4>
+                                    <p class="bit-20"><?=anchor('http://www.facebook.com/sharer.php?u='.current_url(), img('images/assets/face.jpg'), array('target'=>'_blank'));?></p>
+                                    <p class="bit-20"><?=anchor('http://twitter.com/?status='.urlencode('Boletín Septiembre 2014 ').current_url(), img('images/assets/twitter.jpg'), array('target'=>'_blank'));?></h4></p>
+                                </div>
+                                <div class="opinion bit-1 centered blue-square rounded shadow clearfix">
+                                    <h3 class="bit-1">Queremos<br /><span class="red-text">conocer</span><br />m&aacute;s de ti.</h3>
+                                    <p class="bit-1">Ay&uacute;danos con esta peque&ntilde;a encuesta</p>
+                                    <p class="bit-1"><?=anchor('opinion?email='.$this->session->userdata('email'), 'Da clic aqu&iacute;');?></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="frame">
+                            <div class="bit-3 navy-border">
+                                <div class="square dark-text shadow gray-border ">
+                                    <div class="bit-1 dark-text inner-square first">
+                                        <?=heading('<span>11</span> razones para dejar de fumar', 2, 'class="bit-1 centered"' );?>
+                                        <p class="bit-30"><?=img('images/assets/2015/julio/home/no-fumar.png', 'No fumar', [ 'class' => 'bit-1' ] );?></p>
+                                        <p class="bit-70">El hábito de fumar es la principal causa evitable de enfermedades y provoca la muerte prematura.</p>
+                                        <p class="bit-40"><?=anchor( '2015/octubre/salud', img( 'images/assets/2015/julio/home/leer-mas.png', 'Leer más' ) );?></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="bit-3 navy-border">
+                                <div class="square dark-text shadow gray-border">
+                                    <div class="bit-1 dark-text inner-square second">
+                                        <?=heading( 'Falta cultura del ahorro en México', 3, 'class="bit-1 centered"' );?>
+                                        <p class="bit-30"><?=img( 'images/assets/2015/julio/home/marrano.png', 'Alcancia' );?></p>
+                                        <p class="bit-70">Sólo el 16% de los mexicanos ahorra.</p>
+                                        <p class="bit-40"><?=anchor( '2015/octubre/futuro', img( 'images/assets/2015/julio/home/leer-mas.png', 'Leer más' ) );?></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="bit-3">
+                                <div class="square dark-text shadow gray-border">
+                                    <div class="bit-1 inner-square third">
+                                        <?=heading( 'Por tu seguridad, evita el celular al volante.', 3, 'class="bit-1 centered"' );?>
+                                        <p class="bit-30"><?=img( 'images/assets/2015/julio/home/no-telefono.png' );?></p>
+                                        <p class="bit-70">Usar el celular al volante ha hecho que se registre un aumento en accidentes de tránsito.</p>
+                                        <p class="bit-40"><?=anchor( '2015/octubre/auto', img( 'images/assets/2015/julio/home/leer-mas.png', 'Leer más' ) );?></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="frame">
+                            <div class="bit-3">
+                                <div class="small-square">
+                                    <div class="bit-1 inner-square first" style="background-color:#e8e7e5;">
+                                        <h3 class="bit-1 centered">¡Ya puedes pagar tu póliza desde dispositivos móviles!</h3>
+                                        <h4 class="bit-60 centered">Fácil y en todo momento.</h4>
+                                        <p class="bit-40 centered">
+                                            <?=img( 'images/assets/2015/inicio/dispositivos.png' );?>
+
+                                        </p>
+                                        <p class="bit-60 centered">
+                                            <?=anchor('https://cajaaxa.mitec.com.mx/cua/inicio.do?method=loginAgente&perfil=cliente', img('images/assets/btn_descubremas.png'));?>
+
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="bit-3">
+                                <div class="small-square">
+                                    <div class="bit-1 inner-square second" style="background-color:#dcd9d6;">
+                                        <p class="bit-40">
+                                            <?=img( 'images/assets/2015/inicio/call_center.png' );?>
+
+                                        </p>
+                                        <h3 class="bit-60 centered">¿Insatisfecho con nuestro servicio?</h3>
+                                        <p class="bit-60 centered">
+                                            <?=anchor( 'http://portal.axa.mx/Personas/ServicioAXA/Contactanos/Paginas/Quejas.aspx', img('images/assets/btn_hazclicaqui.png'), [ 'anchor'=>'_blank', 'class' =>'clearfix' ] );?>
+
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="bit-3">
+                                <div class="small-square" style="background-color:#e8e7e5;">
+                                    <div class="bit-1 inner-square third">
+                                        <p class="bit-40 centered">
+                                            <?=img( 'images/assets/2015/julio/home/indigena.png' );?>
+
+                                        </p>
+                                        <h3 class="bit-60"><strong>Fundación AXA</strong>, desarrolla el Primer Centro Integral Demostrativo y de Voluntariado, <strong>Casa de la Mujer de Ganzdá</strong>, en Acambay, México.</h3>
+                                        <p class="bit-60 centered">
+                                            <?=anchor( 'https://youtu.be/NVWhGQIb04g', img( 'images/assets/2015/julio/home/conoce-mas-aqui.png' ), [ 'target' => '_blank' ] );?>
+
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="axa_container_division"></div>
