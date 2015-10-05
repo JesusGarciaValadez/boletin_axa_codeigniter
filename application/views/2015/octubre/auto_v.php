b29568134619131556e6d71d7c2efd577be838fa
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2015-10-05 20:35:51 +0000

    AXA Touch - October 2015 bulletin - Home section - Adding markup for home section

--- application/views/2015/octubre/auto_v.php
+++ application/views/2015/octubre/auto_v.php
@@ -1,56 +1,120 @@
-                <div class="container auto">
-                    <div class="frame bit-1 title clearfix">
-                        <div class="bit-80 clearfix">
-                            <h1>Por tu seguridad,<br /> <span><strong>evita el celular</strong> <br />al volante</span></h1>
-                        </div>
-                        <div class="bit-20 clearfix">
-                            <?=img( [ 'src' => 'images/assets/2015/julio/auto/telefono-prohibido.png', 'alt' => 'Vacaciones sobre ruedas' ] );?>
-                        </div>
-                    </div>
-                    <div class="frame bit-1 section-1 clearfix">
-                        <div class="bit-60 clearfix">
-                            <p>El uso del celular al volante ha hecho que se registre un importante aumento en accidentes de tránsito, todo debido a la falta de atención de los conductores.</p>
-                        </div>
-                        <div class="bit-40 clearfix">
-                            <?=img( [ 'src' => 'images/assets/2015/julio/auto/choque.png', 'alt' => 'Choque entre automóviles' ] );?>
-                        </div>
-                    </div>
-                    <div class="frame bit-1 section-2 clearfix">
-                        <hr class="bit-90">
-                        <p class="bit-90">Sabías que 7 de cada 10 accidentes son derivados de los distractores, siendo el celular el más común de ellos.</p>
-                        <hr class="bit-90">
-                    </div>
-                    <div class="frame bit-1 section-3 clearfix">
-                        <h2>Principales distractores que causan accidentes</h2>
-                        <?=img( [ 'src' => 'images/assets/2015/julio/auto/distractores-que-causan-accidentes.jpg', 'alt' => 'Principales distractores que causan accidentes' ] );?>
-                        <p class="bit-1">Investigaciones del Centro de Experimentación y Seguridad Vial México señalan que los accidentes que se habían reducido con el Programa Conduce Sin Alcohol, se vuelven a incrementar debido al uso de teléfonos frente al volante.</p>
-                    </div>
-                    <div class="frame bit-1 section-4 clearfix">
-                        <hr class="bit-90">
-                        <p class="bit-90">Las personas que utilizan el teléfono celular mientras conducen tienen cuatro veces más posibilidades de sufrir un accidente.</p>
-                        <hr class="bit-90">
-                    </div>
-                    <div class="frame bit-1 section-5 clearfix">
-                        <hr class="bit-90">
-                        <p class="bit-90">Hablar por celular reduce el tiempo de reacción un 9%</p>
-                        <hr class="bit-90">
-                    </div>
-                    <div class="frame bit-1 section-6 clearfix">
-                        <div class="bit-80">
-                            <p class="bit-1">El uso del manos libres tampoco es una solución segura, ya que aunque este dispositivo permite poner ambas manos sobre el volante, la falta de atención al camino sigue siendo la misma.</p>
-                        </div>
-                        <div class="bit-20">
-                            <?=img( [ 'src' => 'images/assets/2015/julio/auto/audifonos.png', 'alt' => 'Audífonos' ] );?>
-                        </div>
-                        <p class="bit-1">¡No arriesgues tu vida y la de los demás por contestar el teléfono!</p>
-                    </div>
-                    <div class="frame bit-1 section-7 clearfix">
-                        <div class="bit-80">
-                            <p class="bit-1">Recomendaciones:<br />Si recibes una llamada mientras conduces te sugerimos detenerte en un lugar seguro o pedir que te llamen después.</p>
-                        </div>
-                        <div class="bit-20">
-                            <?=img( [ 'src' => 'images/assets/2015/julio/auto/alerta.png', 'alt' => 'Alerta', 'class' => 'centered' ] );?>
-                        </div>
-                    </div>
+                <div class="container hogar">
+                    <div class="bit-1 frame clearfix title">
+                        <?= img( [ 'src' => 'images/assets/2015/octubre/auto/como-conducir-ante-climas-adversos.png', 'alt' => 'Como conducir ante climas adversos', 'class' => 'bit-1' ] ); ?>
+                    </div><!-- Cómo conducir ante cliomas adversos -->
+                    <div class="bit-1 clearfix section-1 frame">
+                        <p class="bit-1">Al <strong>conducir</strong> es común <strong>enfrentarte</strong> con <strong>diversas condiciones climatológicas</strong> que pueden ser <strong>adversas</strong> y <strong>retrasar</strong> o <strong>impedir tu viaje</strong>, e incluso <strong>poner</strong> en <strong>riesgo</strong> tu <strong>seguridad</strong>. Por ello, te damos algunos <strong>consejos</strong> para prever <strong>accidentes</strong> ante estos escenarios:</p>
+                    </div><!-- Las afectaciones en el hogar provocadas por fenómenos naturales se pueden evitar… -->
+                    <div class="bit-1 clearfix section-2 frame">
+                        <?= img( [ 'src' => 'images/assets/2015/octubre/auto/lluvia.png', 'alt' => 'Lluvia', 'class' => 'centered' ] ); ?>
+                        <?= img( [ 'src' => 'images/assets/2015/octubre/auto/linea.png', 'alt' => 'Línea', 'class' => 'bit-1' ] ); ?>
+                    </div><!-- Lluvia -->
+                    <div class="bit-1 clearfix section-3 frame">
+                        <ul class="clearfix bit-1">
+                            <li class="bit-3 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/auto/reloj-24-horas.png', 'alt' => 'reloj 24 horas', 'class' => 'centered' ] ); ?>
+                                <p>Disminuye la velocidad, al menos a la mitad de lo permitido en seco, pues la lluvia reduce la adherencia de los neumáticos sobre el asfalto.</p>
+                            </li>
+                            <li class="bit-3 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/auto/fila-de-autos.png', 'alt' => 'Fila de autos', 'class' => 'centered' ] ); ?>
+                                <p>Mantén una distancia considerable respecto al auto de adelante, ya que el tiempo de frenado es más lento.</p>
+                            </li>
+                            <li class="bit-3 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/auto/carro-en-carretera.png', 'alt' => 'Carro en carretera', 'class' => 'centered' ] ); ?>
+                                <p>Evita hacer movimientos bruscos, pues podrías perder fácilmente el control del vehículo.</p>
+                            </li>
+                            <li class="bit-3 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/auto/pie-frenando.png', 'alt' => 'pie frenando', 'class' => 'centered' ] ); ?>
+                                <p>Frena suavemente y con pisadas cortas, para secar la humedad de las pastillas y no bloquear la dirección.</p>
+                            </li>
+                            <li class="bit-3 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/auto/faros.png', 'alt' => 'Faros', 'class' => 'centered' ] ); ?>
+                                <p>Si la lluvia es torrencial, es recomendable que enciendas las luces, faros de niebla e intermitentes. Así podrás ser visto por los demás conductores.</p>
+                            </li>
+                            <li class="bit-3 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/auto/auto-parabrisas.png', 'alt' => 'auto parabrisas', 'class' => 'centered' ] ); ?>
+                                <p>Evita que los cristales se empañen activando el aire acondicionado con aire frío hacia el parabrisas.</p>
+                            </li>
+                        </ul>
+                        <ul class="clearfix bit-70 centered">
+                            <li class="bit-1 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/auto/persona-manejando.png', 'alt' => 'Persona manejando', 'class' => 'centered' ] ); ?>
+                                <p>En caso de "sentir" que el auto "flota", no frenes de golpe, solo deberás levantar el pie del acelerador hasta que se reestablezca el control direccional del mismo.</p>
+                            </li>
+                        </ul>
+                    </div><!-- Recomendaciones con lluvia -->
+                    <div class="bit-1 clearfix section-4 frame">
+                        <?= img( [ 'src' => 'images/assets/2015/octubre/auto/viento.png', 'alt' => 'Viento', 'class' => 'centered' ] ); ?>
+                        <?= img( [ 'src' => 'images/assets/2015/octubre/auto/linea.png', 'alt' => 'Línea', 'class' => 'bit-1' ] ); ?>
+                    </div><!-- Viento -->
+                    <div class="bit-1 clearfix section-5 frame">
+                        <ul class="clearfix bit-1">
+                            <li class="bit-3 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/auto/espejo-retrovisor.png', 'alt' => 'Espejo retrovisor', 'class' => 'centered' ] ); ?>
+                                <p>Mantente alerta ante objetos que pudieran obstruir tu visibilidad.</p>
+                            </li>
+                            <li class="bit-3 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/auto/volante.png', 'alt' => 'Volante', 'class' => 'centered' ] ); ?>
+                                <p>Mantente alerta ante objetos que pudieran obstruir tu visibilidad.</p>
+                            </li>
+                            <li class="bit-3 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/auto/velocimetro.png', 'alt' => 'Velocímetro', 'class' => 'centered' ] ); ?>
+                                <p>Reduce la velocidad para que el motor ayude a contrarrestar la fuerza del viento.</p>
+                            </li>
+                        </ul>
+                        <ul class="clearfix bit-70 centered">
+                            <li class="bit-1 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/auto/carro-azul-en-carretera.png', 'alt' => 'Auto en carretera', 'class' => 'centered' ] ); ?>
+                                <p>Para lidiar contra el viento lo que necesitas es potencia, no velocidad. El propio peso del auto te permitirá tener un mejor agarre en el pavimento.</p>
+                            </li>
+                        </ul>
+                    </div><!-- Recomendaciones con viento -->
+                    <div class="bit-1 clearfix section-6 frame">
+                        <?= img( [ 'src' => 'images/assets/2015/octubre/auto/niebla.png', 'alt' => 'Niebla', 'class' => 'centered' ] ); ?>
+                        <?= img( [ 'src' => 'images/assets/2015/octubre/auto/linea.png', 'alt' => 'Línea', 'class' => 'bit-1' ] ); ?>
+                    </div><!-- Niebla -->
+                    <div class="bit-1 clearfix section-7 frame">
+                        <ul class="clearfix bit-1">
+                            <li class="bit-3 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/auto/carro-rojo-frente.png', 'alt' => 'Auto detenido', 'class' => 'centered' ] ); ?>
+                                <p>No te detengas en la mitad del camino.</p>
+                            </li>
+                            <li class="bit-3 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/auto/carro-rojo-con luces.png', 'alt' => 'Faros de niebla', 'class' => 'centered' ] ); ?>
+                                <p>Utiliza faros de niebla. Ayudan a hacer que tu vehículo sea más visible para los demás conductores.</p>
+                            </li>
+                            <li class="bit-3 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/auto/carro-rojo.png', 'alt' => 'Carro con luces altas', 'class' => 'centered' ] ); ?>
+                                <p>Evita usar las luces altas. Podrían reflejarse en la niebla y cegarte temporalmente.</p>
+                            </li>
+                        </ul>
+                        <ul class="clearfix bit-1 centered">
+                            <li class="bit-2 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/auto/nota-musical.png', 'alt' => 'Auto y notas musicales', 'class' => 'centered' ] ); ?>
+                                <p>Apaga la música y, de preferencia, baja la ventana, así podrás escuchar ruidos que te ayuden a identificar algún peligro.</p>
+                            </li>
+                            <li class="bit-2 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/auto/carro-rojo-veloz.png', 'alt' => 'Auto veloz', 'class' => 'centered' ] ); ?>
+                                <p>Circula siempre por el carril derecho. Si la niebla es muy densa, toma como referencia la línea continua de la derecha del carril.</p>
+                            </li>
+                        </ul>
+                    </div><!-- Recomendaciones con Niebla -->
+                    <div class="bit-1 clearfix section-8 frame">
+                        <?= img( [ 'src' => 'images/assets/2015/octubre/auto/recuerda-que-un-camino-seguro-se-logra-con-un-conductor-responsable.png', 'alt' => 'Recuerda que un camino seguro se logra con un responsable', 'class' => 'bit-1 centered' ] ); ?>
+                    </div><!-- Recuerda que un camino seguro se logra con un responsable -->
+                    <div class="bit-1 clearfix section-9 frame">
+                        <ul class="clearfix bit-1">
+                            <li class="bit-2 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/auto/cinturon-de-seguridad.png', 'alt' => 'Cinturón de seguridad', 'class' => 'centered' ] ); ?>
+                                Usa siempre el cinturón de seguridad
+                            </li>
+                            <li class="bit-2 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/auto/lista-y-carros.png', 'alt' => 'Checklist vehícular', 'class' => 'centered' ] ); ?>
+                                Siempre asegúrate que tu vehículo, en general,
+                                esté en orden y buen estado: llantas, frenos,
+                                gasolina, limpiaparabrisas, etc.
+                            </li>
+                        </ul>
+                    </div><!-- Recomendaciones para un camino seguro -->
                     <hr class="bit-1 rule-information bit-1" />
                 </div>
