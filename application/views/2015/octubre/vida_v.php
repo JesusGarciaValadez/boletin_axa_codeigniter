d7aba44db2bb4f712baab9d1507fad049ce887ca
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2015-10-09 17:57:56 +0000

    AXA Touch - Bulletin October 2015 - Life - Markup

---
+++ application/views/2015/octubre/vida_v.php
@@ -0,0 +1,167 @@
+                <div class="container life">
+                    <div class="bit-1 frame clearfix title">
+                        <h1 class="bit-2 centered">
+                            Con base en un estudio <br />del <strong class="text-pink centered">INEGI</strong> (Instituto Nacional <br />de Estadística y Geografía) <br />los porcentajes <br />de gasto promedio <br />en los mexicanos son:
+                        </h1>
+                        <?= img( [ 'src' => 'images/assets/2015/octubre/vida/el-que-ahorra-mas-vive-mejor.png', 'alt' => 'El que ahorra más, vive mejor', 'class' => 'bit-2' ] ); ?>
+                    </div><!-- El que ahorra más, vive mejor -->
+                    <div class="bit-1 clearfix section-1 frame">
+                        <ul class="clearfix bit-80">
+                            <li class="bit-40 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/vida/manzama-treinta-y-cuatro-porciento.png', 'alt' => '34%', 'class' => 'righted' ] ); ?>
+                                <p class="bit-60 lefted">
+                                    Alimentos y bebidas <br />no alcohólicas.
+                                </p>
+                            </li>
+                            <li class="bit-20 centered"></li>
+                            <li class="bit-40 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/vida/libro-catorce-porciento.png', 'alt' => '14%', 'class' => 'lefted' ] ); ?>
+                                <p class="bit-60 lefted">
+                                    Educación <br />y esparcimiento.</strong>
+                                </p>
+                            </li>
+                            <li class="bit-40 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/vida/casa-quince-porciento.png', 'alt' => '15%', 'class' => 'righted' ] ); ?>
+                                <p class="bit-60 lefted">
+                                    Vivienda y sus <br />servicios generales.
+                                </p>
+                            </li>
+                            <li class="bit-20 centered"></li>
+                            <li class="bit-40 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/vida/mandil-cinco-porciento.png', 'alt' => '5%', 'class' => 'lefted' ] ); ?>
+                                <p class="bit-60 lefted">
+                                    Vestido <br />y calzado.
+                                </p>
+                            </li>
+                            <li class="bit-40 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/vida/camioneta-diez-y-nueve-porciento.png', 'alt' => '19%', 'class' => 'righted' ] ); ?>
+                                <p class="bit-60 lefted">
+                                    Transporte <br /> y comunicaciones.
+                                </p>
+                            </li>
+                            <li class="bit-20 centered"></li>
+                            <li class="bit-40 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/vida/muela-nueve-porciento.png', 'alt' => '9%', 'class' => 'lefted' ] ); ?>
+                                <p class="bit-60 lefted">
+                                    Cuidados a la salud <br />y personales.
+                                </p>
+                            </li>
+                        </ul>
+                        <ul class="clearfix bit-20">
+                            <li class="bit-1 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/vida/moneda-cuatro-porciento.png', 'alt' => '4%', 'class' => 'centered' ] ); ?>
+                                <p class="bit-1 centered">Transferencia de gasto.</p>
+                            </li>
+                        </ul>
+                    </div><!-- Octubre, mundialmente, es el mes de la lucha contra el cáncer de mama, y tiene… -->
+                    <div class="bit-1 clearfix section-2 frame">
+                        <?= img( [
+                                'src' => 'images/assets/2015/octubre/vida/resultado-que-deja-un-cero-porciento-para-el-ahorro.png',
+                                'alt' => 'Resultado que deja un cero porciento para el ahorro.',
+                                'class' => 'centered'
+                            ] );
+                        ?>
+                    </div><!-- Resultado que deja un cero porciento para el ahorro. -->
+                    <div class="bit-1 clearfix section-3 frame">
+                        <?= img( [ 'src' => 'images/assets/2015/octubre/vida/linea-horizontal.png', 'alt' => 'Separador', 'class' => 'centered' ] ); ?>
+                        <p class="bit-1 centered">
+                            De hecho, según una encuesta de la CONSAR (Comisión Nacional del Sistema de Ahorro <br />para el Retiro) <strong>En México el 66% de los habitantes no ahorran , 7 de cada 10 sílo hacen</strong> <br />pero usan el dinero para <strong>afrontar una emergencia</strong> y sólo <strong>el 7% lo hace pensando en <br />su vejez.</strong> Mientras los que <strong>no ahorran, el 49% es porque no les alcanza, el 28% porque <br />tienen otros gastos y el 19% por falta de hábito.</strong>
+                        </p>
+                        <?= img( [ 'src' => 'images/assets/2015/octubre/vida/linea-horizontal.png', 'alt' => 'Separador', 'class' => 'centered' ] ); ?>
+                    </div><!-- De hecho, según una encuesta de la CONSAR (Comisión Nacional del Sistema de Ahorro… -->
+                    <div class="bit-1 clearfix section-4 frame">
+                        <?= img( [
+                                'src' => 'images/assets/2015/octubre/vida/el-habito-de-ahorrar-beneficia-en-varios-aspectos-como.png',
+                                'alt' => 'El hábito de ahorrar beneficia en varios aspectos como',
+                                'class' => 'centered'
+                            ] );
+                        ?>
+                    </div><!-- El hábito de ahorrar beneficia en varios aspectos, como: -->
+                    <div class="bit-1 clearfix section-5 frame">
+                        <ul class="clearfix bit-1">
+                            <li class="bit-5 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/vida/casa-icono.png', 'alt' => 'Manzana', 'class' => 'centered' ] ); ?>
+                                <p class="bit-80">La posibilidad <br />de comprar<br /> una casa.</p>
+                            </li>
+                            <li class="bit-5 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/vida/ambulancia-icono.png', 'alt' => 'Bicicleta', 'class' => 'centered' ] ); ?>
+                                <p class="bit-80">Cubrir emergencias <br />e imprevistos.</p>
+                            </li>
+                            <li class="bit-5 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/vida/libros-icono.png', 'alt' => 'Cigarro', 'class' => 'centered' ] ); ?>
+                                <p class="bit-80">Proveer <br />la educación <br />de tus hijos.</p>
+                            </li>
+                            <li class="bit-5 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/vida/avion-icono.png', 'alt' => 'Mastografía', 'class' => 'centered' ] ); ?>
+                                <p class="bit-80">Tomar vacaciones <br />y viajar.</p>
+                            </li>
+                            <li class="bit-5 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/vida/alcancia-icono.png', 'alt' => 'Mastografía', 'class' => 'centered' ] ); ?>
+                                <p class="bit-80">Tener una jubilación <br />de calidad.</p>
+                            </li>
+                        </ul>
+                    </div><!-- Beneficios del hábito de ahorrar. -->
+                    <div class="bit-1 clearfix section-6 frame centered">
+                        <p class="bit-80 centered">
+                            Sin embargo, no siempre es fácil saber cuál es la cantidad ideal para ahorrar. Forbes estipula que <br /><strong>lo ideal</strong> es del <strong>10% al 20% de tu ingreso total,</strong> pero puede <strong>variar</strong> según <strong>objetivo, edad, <br />situación</strong> y <strong>presupuesto</strong>. Por eso, dejamos unas recomendaciones para distintas etápas de vida:
+                        </p>
+                    </div><!-- Sin embargo, no siempre es fácil saber cuál es la cantidad ideal para ahorrar… -->
+                    <div class="bit-1 clearfix section-7 frame">
+                        <ul class="clearfix bit-1">
+                            <li class="bit-3">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/vida/hombre-camisa-verde.png', 'alt' => 'Hombre camisa verde', 'class' => 'lefted' ] ); ?>
+                                <p class="bit-80 lefted">Si tienes 18 años, ahorra <br />el 10% para iniciar <br />el hábito.</p>
+                            </li>
+                            <li class="bit-3 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/vida/hombre-camisa-roja.png', 'alt' => 'Hombre camisa roja', 'class' => 'lefted' ] ); ?>
+                                <p class="bit-80 lefted">Si eres soltero, <br />ahorra del 25% al 30%.</p>
+                            </li>
+                            <li class="bit-3 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/vida/hombre-camisa-azul.png', 'alt' => 'Hombre camisa azul', 'class' => 'lefted' ] ); ?>
+                                <p class="bit-80 lefted">Si tienes entre 28 y 38 <br />años, ahorra el 15%.</p>
+                            </li>
+                            <li class="bit-2 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/vida/hombre-con-portafolio.png', 'alt' => 'Hombre con portafolios', 'class' => 'lefted' ] ); ?>
+                                <p class="bit-80 lefted">Si tienes entre 38 y 48 <br />años, ahorra el 20%.</p>
+                            </li>
+                            <li class="bit-2 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/vida/hombre-mayor.png', 'alt' => 'Hombre mayor', 'class' => 'lefted' ] ); ?>
+                                <p class="bit-80 lefted">Si tienes entre 48 <br /> y 58 años, ahorra el 30%.</p>
+                            </li>
+                        </ul>
+                    </div><!-- Recomendaciones para distintas etápas de vida -->
+                    <div class="bit-1 clearfix section-8 frame">
+                        <?= img( [
+                                'src' => 'images/assets/2015/octubre/vida/tambien-puedes.png',
+                                'alt' => 'También puedes',
+                                'class' => 'centered'
+                            ] );
+                        ?>
+                    </div><!-- También puedes -->
+                    <div class="bit-1 clearfix section-9 frame">
+                        <ul class="clearfix bit-1">
+                            <li class="bit-3">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/vida/cigarro-icono.png', 'alt' => 'Cigarro', 'class' => 'centered' ] ); ?>
+                                <p class="bit-80 centered">Eliminar los productos innecesarios de tu presupuesto.</p>
+                            </li>
+                            <li class="bit-3 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/vida/calendario-icono.png', 'alt' => 'Calendario', 'class' => 'centered' ] ); ?>
+                                <p class="bit-80 centered">Establecer metas de ahorro razonables.</p>
+                            </li>
+                            <li class="bit-3 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/vida/monedas-icono.png', 'alt' => 'Monedas', 'class' => 'centered' ] ); ?>
+                                <p class="bit-80 centered">Comparar precios y marcas antes de tomar una elección de compra.</p>
+                            </li>
+                        </ul>
+                        <ul class="clearfix bit-70 centered">
+                            <li class="bit-1 centered">
+                                <?= img( [ 'src' => 'images/assets/2015/octubre/vida/carro-icono.png', 'alt' => 'Carro', 'class' => 'centered' ] ); ?>
+                                <p>Reducir el consumo energético. Puedes apagar los electrónicos cuando no los uses u optar por moverte en bicicleta o transporte pú́blico en lugar de en el auto.</p>
+                            </li>
+                        </ul>
+                    </div><!-- Continuación de recomendaciones para distintas etápas de vida -->
+                    <div class="bit-1 clearfix foot-notes frame centered">
+                        <p class="bit-1">Fuente: INEGI y Banco de México, Consar.</p>
+                    </div><!-- Fuente: INEGI y Banco de México, Consar. -->
+                    <hr class="bit-1 rule-information bit-1" />
+                </div>
